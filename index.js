bac98b306bbda09f13824cd5e97faf805c04d33b
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-07-27 19:54:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = {ingredient: 'amount'}
+var recipes = {'ingredient': 'amount'}
 
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, {[key]:value});
