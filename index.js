01e7ba923445d5ac8a1080301ae1fe1348bda9eb
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-07-27 19:27:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var recipes = Object.assign(recipes);
+  delete recipes.ingredient;
   return recipes;
 }
 
